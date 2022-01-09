ec4da5c5035f76c909724616e6d98b1f9f17fccf
dotnetwebsites <kpljain21@gmail.com>
2022-01-10 00:29:09 +0530

    Initial commit

---
+++ finocred/Controllers/BusinessLoanController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace finocred.web.Controllers
+{
+    public class BusinessLoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Eligibility()
+        {
+            return View();
+        }
+
+        public IActionResult EmiCalculator()
+        {
+            return View();
+        }
+
+        public IActionResult Documents()
+        {
+            return View();
+        }
+
+        public IActionResult Apply()
+        {
+            return View();
+        }
+    }
+}
---
+++ finocred/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace finocred.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
